06fe04b07a995303b30e0b17cce41c7b016a356e
Mothirajha <rajha12@gmail.com>
2017-06-10 18:47:42 +0530

    [Update] basic class

--- app/index.js
+++ app/index.js
@@ -4,3 +4,20 @@ import add from './calculation.js'
 
 console.log(add(1, 3));
 // console.log(sub(1, 3));
+
+
+//Class
+class Animal {
+  constructor(name, height) {
+    this.name = name;
+    this.height = height;
+  }
+
+  hello(){
+    console.log(`Hi I'am ${this.name} from Animal class`);
+  }
+}
+
+let king = new Animal("Lion", "4.5");
+console.log(king);
+console.log(king.hello());
