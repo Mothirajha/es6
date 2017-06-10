876d8aefc3c066987e9e2b4dab614362e4d2c15d
Mothirajha <rajha12@gmail.com>
2017-06-10 12:47:03 +0530

    [Update] var, let, const and block variables on es6

--- app/index.js
+++ app/index.js
@@ -1,1 +1,50 @@
 console.log("Hello world");
+const numbers = [1,2,2,23,3];
+numbers.push(100);
+console.log(numbers);
+// const doesn't allow to assign variables again but can be updated.
+
+
+// Example - Block scoping
+let limit = 100;
+
+{
+  let limit = 10;
+  console.log('Back stage limit ',limit);
+}
+
+console.log('Overall stage limit ',limit);
+
+
+var vlimit = 100;
+
+{
+  var vlimit = 10;
+  console.log('Back stage vlimit ',vlimit);
+}
+
+console.log('Overall stage vlimit ',vlimit);
+
+
+const climit = 100;
+
+{
+  const climit = 10;
+  console.log('Back stage climit ',climit);
+}
+
+console.log('Overall stage climit ',climit);
+
+
+function hello() {
+  let message = "Hello world!";
+  console.log(message);
+}
+
+function greeting() {
+    let message = "How r you ?";
+    console.log(message);
+}
+
+hello();
+greeting();
