d356bd5bc0467e64a1f6c8a5c0033f47da5e4fb2
Mothirajha <rajha12@gmail.com>
2017-06-10 19:09:09 +0530

    [Update] static method in class

--- app/index.js
+++ app/index.js
@@ -5,9 +5,27 @@ import add from './calculation.js';
 console.log(add(1, 3));
 // console.log(sub(1, 3));
 
+// Class Example
 import Dog from './dog';
-
 
 let inherit = new Dog('Tommy', 2.2, "yellow");
 console.log(inherit);
 inherit.hello();
+
+
+// Static Methods
+
+class Calculator {
+  static multiply(a, b){
+    return a*b;
+  }
+
+  static add(a, b){
+    return a+b;
+  }
+}
+
+let a = Calculator.multiply(10, 2);
+console.log(a);
+let b = Calculator.add(10, 2);
+console.log(b);
