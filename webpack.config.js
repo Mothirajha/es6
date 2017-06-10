6a2bdf4f0fb83f502b6b3678558e63cbce7ab4c6
Mothirajha <rajha12@gmail.com>
2017-06-10 12:27:44 +0530

    [Update] basic local server

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve('./build'),
     filename: 'bundle.js'
-  }
+  },
 
   module: {
     loaders: [
@@ -15,7 +15,8 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  
   devServer: {
     port: 3000,
     contentBase: './build',
