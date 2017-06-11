74c9f26b11f8a6f433b93f460a3805eb2995dae1
Mothirajha <rajha12@gmail.com>
2017-06-11 18:34:07 +0530

    [Update] cleaning up the project folder for react

--- app/wizard.js
+++
@@ -1,9 +0,0 @@
-// Prototypes - Example
-function Wizard(name, house, pet) {
-  this.name = name;
-  this.house = house;
-  this.pet = pet;
-
-  this.greet = () => `I'am ${this.name} from ${this.house}`;
-}
-export default Wizard;
