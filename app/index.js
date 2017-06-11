adcc36527d8f8fa969625d0f49ee00fa3d461c3a
Mothirajha <rajha12@gmail.com>
2017-06-11 17:59:51 +0530

    [Update] ES6 fetch

--- app/index.js
+++ app/index.js
@@ -264,6 +264,13 @@ let p = new Promise((resolve, reject) => {
   // reject("Reject promise data");
 })
 console.log(p);
-p.then(response => console.log(response))
- .catch(error => console.log(error));
-console.log("After promise consumption");
+// p.then(response => console.log(response))
+//  .catch(error => console.log(error));
+// console.log("After promise consumption");
+
+// Es6 - Fetch - API call
+const root = 'https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699'
+
+fetch(root, {method: "GET"})
+ .then(response => response.json())
+ .then(json => console.log(json));
