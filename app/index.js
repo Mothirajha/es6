427414d871008421010b9d4320722c8a68bb3e18
Mothirajha <rajha12@gmail.com>
2017-06-11 19:35:43 +0530

    [Update] components and state

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Global from './components/global';
 
 ReactDom.render(
-  <div>React Application!!! </div>, document.getElementById('root')
+  <Global />, document.getElementById('root')
 );
